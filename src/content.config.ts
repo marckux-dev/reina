f98175577eea43e35658e9b5b30a4a7a186ca37e
marckux-dev <marckux.developer@gmail.com>
2025-12-12 02:09:10 +0100

    Segundo intento subir fotos

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+// import { glob } from "astro/loaders";
 
 const servicesCollection = defineCollection({
   // loader: glob({ pattern: "services/**/*.{md,mdx}", base: "./src/content" }),
