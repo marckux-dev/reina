0ea80044a6da82eed5b304641afac0c5188cd7ad
marckux-dev <marckux.developer@gmail.com>
2025-12-13 10:54:39 +0100

    Intento arreglo JSON-LD de la página de inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [mdx()]
+  site: 'https://reinamultiservicios.es',
+  integrations: [mdx(), sitemap()]
 });
